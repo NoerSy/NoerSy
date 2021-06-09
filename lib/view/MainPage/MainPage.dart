c57f8cde46d728a6340d5350f13dbafe04da78a4
NoerSy <nur.syahfei@gmail.com>
2021-06-10 00:31:18 +0700

    up

--- lib/view/MainPage/MainPage.dart
+++ lib/view/MainPage/MainPage.dart
@@ -29,50 +29,55 @@ class _MainPageState extends State<MainPage> {
                         topRight: Radius.circular(5),
                         bottomRight: Radius.circular(5)),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Stack(
-                        children: [
-                          SelectableText(
-                            "Nur Syahfei",
-                            style: GoogleFonts.odibeeSans(
-                              fontSize: 100,
-                              color: Platecolor.bg2,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 80, left: 220),
-                            child: SelectableText(
-                              "noersy",
+                  child: Padding(
+                    padding: EdgeInsets.only(left:
+                    MediaQuery.of(context).size.width/8,
+                  ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Stack(
+                          children: [
+                            SelectableText(
+                              "Nur Syahfei",
                               style: GoogleFonts.odibeeSans(
-                                fontSize: 35,
+                                fontSize: 100,
                                 color: Platecolor.bg2,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SelectableText(
-                        "Junior Flutter Developer",
-                        style: GoogleFonts.signika(
-                          fontSize: 30,
-                          color: Platecolor.bg2,
-                          fontStyle: FontStyle.italic,
+                            Padding(
+                              padding: const EdgeInsets.only(top: 80, left: 220),
+                              child: SelectableText(
+                                "noersy",
+                                style: GoogleFonts.odibeeSans(
+                                  fontSize: 35,
+                                  color: Platecolor.bg2,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SelectableText(
-                          "GitHub - Linkedin - Twitter - Instagram",
-                          style: GoogleFonts.lato(
-                            fontSize: 18,
+                        SelectableText(
+                          "Junior Flutter Developer",
+                          style: GoogleFonts.signika(
+                            fontSize: 30,
                             color: Platecolor.bg2,
+                            fontStyle: FontStyle.italic,
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SelectableText(
+                            "GitHub - Linkedin - Twitter - Instagram",
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              color: Platecolor.bg2,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
