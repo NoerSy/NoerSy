84e2c8f33a7df8084fc20cc9e65f0b2502912d48
Nur Syahfei <nur.syahfei@gmail.com>
2021-12-01 08:24:28 +0700

    new porto profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myprofile_flutter/view/MainPage/MainPage.dart';
+import 'package:myprofile_flutter/view/main_page/main_page.dart';
 import 'package:url_strategy/url_strategy.dart';
+import 'package:universal_html/html.dart' as html;
 
 void main() {
   setPathUrlStrategy();
@@ -10,10 +11,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final loader = html.document.getElementsByClassName('loader-wrapper');
+    if (loader.isNotEmpty) {
+      loader.first.remove();
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Nur Syahfei',
       home: MainPage(),
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme().copyWith(
+            subtitle1: TextStyle(
+              fontSize: 20.5
+            )
+        ),
+      ),
     );
   }
 }
+
