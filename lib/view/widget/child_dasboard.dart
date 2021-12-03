f2b5f4d5c8933d4bc87d8da279758132eeeae40a
Nur Syahfei <nur.syahfei@gmail.com>
2021-12-03 20:12:55 +0700

    fix timming

--- lib/view/widget/child_dasboard.dart
+++ lib/view/widget/child_dasboard.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/cupertino.dart';
 
-class ChildDashboard extends StatelessWidget {
+class ChildDashboard extends StatefulWidget {
   final int index;
   final int current;
   final Widget child;
-  const ChildDashboard({Key? key, required this.index, required this.child, required this.current}) : super(key: key);
+
+  const ChildDashboard(
+      {Key? key,
+      required this.index,
+      required this.child,
+      required this.current})
+      : super(key: key);
+
+  @override
+  State<ChildDashboard> createState() => _ChildDashboardState();
+}
+
+class _ChildDashboardState extends State<ChildDashboard> {
+  bool _isDisplayed = false;
+
+  void startDelayed() async {
+    if (widget.current == widget.index) {
+      Future.delayed(const Duration(milliseconds: 600)).then((value) {
+        setState(() {
+          _isDisplayed = true;
+        });
+      });
+    } else {
+      setState(() {
+        _isDisplayed = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    startDelayed();
     return AnimatedOpacity(
-      duration: current == index ? const Duration(seconds: 2) : const Duration(milliseconds: 600),
-      opacity: current == index ? 1 : 0,
-      child: child,
+      duration: const Duration(milliseconds: 600),
+      opacity: _isDisplayed ? 1 : 0,
+      child: widget.child,
     );
   }
 }
