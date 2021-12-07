aa395b5968a347577543e67b57f14679a831445b
Nur Syahfei <nur.syahfei@gmail.com>
2021-12-07 16:44:50 +0700

    animated show case project 1.2

--- lib/view/page/featured_page.dart
+++ lib/view/page/featured_page.dart
@@ -38,8 +38,6 @@ class _FeaturedPageState extends State<FeaturedPage> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              for (var item in List<int>.generate(
-                  constraints.minWidth < 1500 ? 1 : 2, (index) => index++))
                 ProjectCard(
                   constraints: constraints,
                 ),
@@ -66,14 +64,13 @@ class ProjectCard extends StatefulWidget {
 }
 
 class _ProjectCardState extends State<ProjectCard> {
-  static final ScrollController _controller = ScrollController(initialScrollOffset: 1.0);
+  static final ScrollController _controller = ScrollController();
   static double _offfset = 1.0;
   static bool _atEdnge = false;
 
   @override
   void initState() {
-
-    Timer.periodic(Duration(seconds: _atEdnge ? 1 : 2), (_) {
+    Timer.periodic(const Duration(seconds: 1), (_) {
       if (_controller.position.atEdge) {
         setState((){
           _offfset = 1.0;
@@ -85,8 +82,7 @@ class _ProjectCardState extends State<ProjectCard> {
           _atEdnge = false;
         });
       }
-
-      _controller.animateTo(_offfset, duration: const Duration(seconds: 2), curve: _atEdnge ? Curves.ease : Curves.linear);
+      _controller.animateTo(_offfset, duration: Duration(seconds: _atEdnge ? 1 : 2), curve: _atEdnge ? Curves.ease : Curves.linear);
     });
     super.initState();
   }
@@ -101,88 +97,85 @@ class _ProjectCardState extends State<ProjectCard> {
           color: Platecolor.bg2.withOpacity(0.1),
           borderRadius: BorderRadius.circular(10),
         ),
-        child: Scrollbar(
-          controller: _controller,
-          isAlwaysShown: true,
-          child: Stack(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(4.0),
-                alignment: Alignment.centerLeft,
-                decoration: BoxDecoration(
-                  color: Platecolor.bg2.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.only(
-                    bottom: 12.0,
-                    top: 8.0,
-                    right: 8.0,
-                    left: 8.0,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    controller: _controller,
-                    child: Row(
-                      children: [
-                        Image.asset('assets/images/ajari/1.png'),
-                        Image.asset('assets/images/ajari/2.png'),
-                        Image.asset('assets/images/ajari/3.png'),
-                        Image.asset('assets/images/ajari/4.png'),
-                        Image.asset('assets/images/ajari/5.png'),
-                        Image.asset('assets/images/ajari/6.png'),
-                        Image.asset('assets/images/ajari/7.png'),
-                        Image.asset('assets/images/ajari/8.png'),
-                      ],
-                    ),
-                  ),
-                ),
+        child: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(4.0),
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Platecolor.bg2.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10),
               ),
-              Container(
-                margin: const EdgeInsets.all(4.0),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 12.0,
-                  vertical: 22.0,
+              child: Container(
+                padding: const EdgeInsets.only(
+                  bottom: 12.0,
+                  top: 8.0,
+                  right: 8.0,
+                  left: 8.0,
                 ),
-                alignment: Alignment.topCenter,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Platecolor.bg3.withOpacity(0.7),
-                      Colors.transparent,
-                    ],
-                  ),
                 ),
                 child: SingleChildScrollView(
-                  child: Column(
-                    children: const [
-                      Text(
-                        "Ajari",
-                        style: TextStyle(
-                          fontSize: 46.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 26.0),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 26.0),
-                        child: Text(
-                          "Ajari is a mobile-based application created to facilitate the Al-Quran Learning Place (TPQ) for help teachers teach students and students learn read Iqro based on an independent learning process guided by the teacher.",                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                      ),
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller,
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/ajari/1.png'),
+                      Image.asset('assets/images/ajari/2.png'),
+                      Image.asset('assets/images/ajari/3.png'),
+                      Image.asset('assets/images/ajari/4.png'),
+                      Image.asset('assets/images/ajari/5.png'),
+                      Image.asset('assets/images/ajari/6.png'),
+                      Image.asset('assets/images/ajari/7.png'),
+                      Image.asset('assets/images/ajari/8.png'),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 12.0,
+              ),
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Platecolor.bg3.withOpacity(0.7),
+                    Platecolor.bg3.withOpacity(0.4),
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: const [
+                    Text(
+                      "Ajari",
+                      style: TextStyle(
+                        fontSize: 46.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 26.0),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 26.0),
+                      child: Text(
+                        "Ajari is a mobile-based application created to facilitate the Al-Quran Learning Place (TPQ) for help teachers teach students and students learn read Iqro based on an independent learning process guided by the teacher.",                          textAlign: TextAlign.justify,
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
